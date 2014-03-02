9f4cb18657659de461eeeea3995d0a5164c540d9
Miguel Palhas <mpalhas@gmail.com>
2014-03-02 15:29:41 +0000

    Tutorial done

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    void Update() {
+        transform.Rotate(new Vector3(13, 30, 45) * Time.deltaTime);
+    }
+}
